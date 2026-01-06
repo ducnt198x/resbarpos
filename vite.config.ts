34945ab658f7d570c23a3e80de5b4b555c35a472
ducnt198x <ducnt198x@gmail.com>
2026-01-06 10:12:10 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
         base: '/MyPOS/',
-      server: {
+      server: { 
         port: 3000,
         host: '0.0.0.0',
       },
